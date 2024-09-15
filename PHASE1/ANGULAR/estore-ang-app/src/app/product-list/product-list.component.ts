708988f6234359c29b15c4a879abe694cb95469d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-15 21:29:45 +0530

    updates

--- PHASE1/ANGULAR/estore-ang-app/src/app/product-list/product-list.component.ts
+++ PHASE1/ANGULAR/estore-ang-app/src/app/product-list/product-list.component.ts
@@ -16,10 +16,10 @@ export class ProductListComponent {
   constructor() {
 
     this.products = [];
-    this.products[0] = new Product("iPhone", 75000, "Great Phone", true, '');
-    this.products[1] = new Product("Nokia", 5000, "Simple Phone", true, '');
-    this.products[2] = new Product("Samsung", 15000, "Good Phone", true, '');
-    this.products[3] = new Product("Motorala", 25000, "Nice Phone", false, '');
+    this.products[0] = new Product("iPhone", 75000, "Great Phone", true, 'images/111.png');
+    this.products[1] = new Product("Nokia", 5000, "Simple Phone", true, 'images/222.png');
+    this.products[2] = new Product("Samsung", 15000, "Good Phone", true, 'images/333.png');
+    this.products[3] = new Product("Motorala", 25000, "Nice Phone", false, 'images/444.png');
 
   }
 
