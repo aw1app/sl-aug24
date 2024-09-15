269d5a220fbf9c105dd6df4730dd6c17f1b734df
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-15 20:16:18 +0530

    updates

--- PHASE1/ANGULAR/estore-ang-app/src/app/product.ts
+++ PHASE1/ANGULAR/estore-ang-app/src/app/product.ts
@@ -4,7 +4,8 @@ export class Product {
         public name: string ='',
         public price: number=100,
         public description: string='',
-        public inStock:boolean=true
+        public inStock:boolean=true,
+        public imagePath:string='',
       ) {
         
       }
