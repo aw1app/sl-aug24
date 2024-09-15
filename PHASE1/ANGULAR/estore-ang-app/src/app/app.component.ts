997bb767a8b05850e374d08d599016a683e4a556
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-15 20:53:28 +0530

    updates

--- PHASE1/ANGULAR/estore-ang-app/src/app/app.component.ts
+++ PHASE1/ANGULAR/estore-ang-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from "./product/product.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet, ProductComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PHASE1/ANGULAR/estore-ang-app/src/app/product-list/product-list.component.ts
+++ PHASE1/ANGULAR/estore-ang-app/src/app/product-list/product-list.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { Product } from '../product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
-products!:Product[];
+  products!: Product[];
 
-constructor(){
-  
-  this.products = [];
-  this.products[0] = new Product("iPhone", 75000, "Great Phone", true, '');
-  this.products[1] = new Product("Nokia", 5000, "Great Phone", true, '');
-  this.products[2] = new Product("Samsung", 15000, "Great Phone", true, '');
-  this.products[3] = new Product("Motorala", 25000, "Great Phone", false, '');
+  constructor() {
 
-}
+    this.products = [];
+    this.products[0] = new Product("iPhone", 75000, "Great Phone", true, '');
+    this.products[1] = new Product("Nokia", 5000, "Simple Phone", true, '');
+    this.products[2] = new Product("Samsung", 15000, "Good Phone", true, '');
+    this.products[3] = new Product("Motorala", 25000, "Nice Phone", false, '');
+
+  }
 
 }
