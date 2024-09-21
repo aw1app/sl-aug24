5b2859466d15a0b2baaeb3a72f2052c69dacd767
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-21 21:49:40 +0530

    new project for services

--- PHASE1/ANGULAR/services-http-forms-routing/src/app/services/http-based-product.service.ts
+++ PHASE1/ANGULAR/services-http-forms-routing/src/app/services/http-based-product.service.ts
@@ -1,13 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpBasedProductService {
 
+  apiBaseUrl:string = "http://localhost:3000/products"
+
 
   constructor(private http: HttpClient) { }
 
-  
+  getAllProducts():Observable<Product[]> {  
+
+    return this.http.get<Product[]>(this.apiBaseUrl);
+   
+   }
+
+
 }
