32814720902591a86419579e3f1707d3c86a3f78
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-22 21:36:13 +0530

    new project for services

--- PHASE1/ANGULAR/services-http-forms-routing/src/app/component/add-product/add-product.component.ts
+++ PHASE1/ANGULAR/services-http-forms-routing/src/app/component/add-product/add-product.component.ts
@@ -30,18 +30,29 @@ export class AddProductComponent {
     );
   }
 
+  
+
 
   addProduct(): void {
     let name = this.productForm.get('productName')?.value;
     let price = this.productForm.get('productPrice')?.value;
 
-    let tempProduct = new Product(0, name, price, '', true, 'images/444.png');
+    let id =this.getRandomInt(1000,10000);
+
+    let tempProduct = new Product(id, name, price, '', true, 'images/444.png');
 
     this.httpbasedProductService.addProduct(tempProduct).subscribe(
 
       (newProd) => console.log("Add a new Product . It's id = " + newProd.id)
 
     )
+  }
+
+
+   getRandomInt(min: number, max: number) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
   }
 
 }
