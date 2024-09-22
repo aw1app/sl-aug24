7f96add3ff4d4c95ab6f287de9e6c70f6651221b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-22 20:57:45 +0530

    new project for services

--- PHASE1/ANGULAR/services-http-forms-routing/src/app/component/add-product/add-product.component.ts
+++ PHASE1/ANGULAR/services-http-forms-routing/src/app/component/add-product/add-product.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpBasedProductService } from '../../services/http-based-product.service';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
+  providers: [HttpBasedProductService],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
+
+  productForm!:FormGroup;
+  httpbasedProductService!: HttpBasedProductService;
+
+  constructor(private fb: FormBuilder, httpbasedProductService: HttpBasedProductService){
+    this.httpbasedProductService = httpbasedProductService;
+
+    this.productForm = this.fb.group(
+
+      {
+        productName:[''],
+        productPrice:[0],
+      }
+
+    );
+  }  
+
+
+  addProduct():void{
+
+  }
 
 }
