4f1b4cc213be1338666117e7be10e1da3d1738b2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-22 22:39:56 +0530

    new project for services

--- PHASE1/ANGULAR/services-http-forms-routing/src/app/app.component.ts
+++ PHASE1/ANGULAR/services-http-forms-routing/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import { AddProductComponent } from './component/add-product/add-product.component';
+import { NotFoundComponent } from './component/not-found-component/not-found-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent, AddProductComponent],
+  imports: [RouterOutlet, ProductListComponent, AddProductComponent, NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PHASE1/ANGULAR/services-http-forms-routing/src/app/app.routes.ts
+++ PHASE1/ANGULAR/services-http-forms-routing/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { NotFoundComponent } from './component/not-found-component/not-found-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+
+    { path: 'product-list', component: ProductListComponent },
+    { path: 'add-product', component: AddProductComponent },
+
+    { path: '*', component: NotFoundComponent },
+
+];
