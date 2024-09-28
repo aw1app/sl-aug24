8fb0f81c403ffa6c64053965419243c254848c59
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-28 22:01:45 +0530

    react updates

--- PHASE1/REACT/my-app/src/components/Product.js
+++ PHASE1/REACT/my-app/src/components/Product.js
@@ -1,14 +1,19 @@
 import { Component } from 'react';
 
 class Product extends Component {
-     z = "Hello";
+     productName = "Samsung Mobile";
 
     render() {
         let x = 10;
 
         return (
 
+            <div>
+
             <p> value of x is {x} </p>
+            <p> value of productName is {this.productName} </p>
+
+            </div>
 
         )
 
