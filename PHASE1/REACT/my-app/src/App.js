a29a83b5d76e237ddf9c8dd349d759823013d715
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-28 21:48:39 +0530

    new project for react

---
+++ PHASE1/REACT/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    // any processing ....
+
+    return (
+      <div className="App">
+
+        Hello React from a Class componet
+      </div>
+    );
+
+  }
+
+
+}
+
+export default App;
