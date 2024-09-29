78aeaa4045a156bd8b2fa93ee865f34b0f5baadc
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-09-29 21:34:29 +0530

    react updates

--- PHASE1/REACT/my-app/src/components/Product.jsx
+++ PHASE1/REACT/my-app/src/components/Product.jsx
@@ -8,28 +8,82 @@ class Product extends Component {
 
     constructor(props) {
         super(props);
-        this.productName = this.props.name;
-        this.productPrice = this.props.price;
+
+        const { name, price } = this.props;
+        this.productName = name;
+        this.productPrice = price;
+
+        // this.productName = this.props.name;
+        //this.productPrice = this.props.price;
 
         this.state = {
-            discount:10,
+            name: this.props.name,
+            price: this.props.price,
+            discount: 10,
             inStock: this.props.avl
         };
+
+        this.xyz = this.buttonClicked.bind(this);
+        this.nameInputChange = this.nameInputChange.bind(this);
+        this.priceInputChange = this.priceInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
     }
 
+    buttonClicked(){
+        console.log("BUTTON CLICKED ");
+    }
+
+    nameInputChange(evt){
+        console.log("name  value CHANGED ");
+        console.log(" value is  " + evt.target.value);
+
+        this.setState( {name:evt.target.value} );
+    }
+
+    priceInputChange(evt){
+        console.log("price value CHANGED ");
+        console.log(" value is  " + evt.target.value);
+        this.setState( {price:evt.target.value} );
+    }
+
+    handleSubmit(evt){
+        console.log("INSIDE FORM SUBMIT HANDLER ");
+
+        //prevent submission so that some validation can be done.
+        evt.preventDefault();
+
+    //validation
+    // console.log(evt.target.name.value);
+    // console.log(evt.target.price.value);
+
+    }
 
     render() {
         let x = 10;
+
+        //this.setState({ discount: 77 })
 
         return (
 
             <div>
 
-                {/* <p> value of x is {x} </p> */}
-                <p> value of productName is {this.productName} </p>
-                <p> value of productPrice is {this.productPrice} </p>
+                <h2> I am PRODUCT CLASS COMPONENT </h2>
+                <p> value of productName is {this.state.name} </p>
+                <p> value of productPrice is {this.state.price} </p>
                 <p> Discount on this product is {this.state.discount} </p>
-                <p>  Availble: {this.state.inStock ==="true" ? "YES": "NO"} </p>
+                <p>  Availble: {this.state.inStock === "true" ? "YES" : "NO"} </p>
+
+                <form action="form-processing.php" method="POST" onSubmit={this.handleSubmit}>
+                    NAME : <input type="text" name="name" value={this.state.name} onChange={this.nameInputChange}/>
+                    <br/>
+                    PRICE: <input type="text" name="price" value={this.state.price} onChange={this.priceInputChange}/>
+                    <br/>
+                    <input type="submit" />
+                </form>
+                
+                <br/>
+                <button onClick={this.xyz}>  Button Click! </button>
 
             </div>
 
--- PHASE1/REACT/my-app/src/components/ProductF.jsx
+++ PHASE1/REACT/my-app/src/components/ProductF.jsx
@@ -2,13 +2,31 @@ import { useState } from "react"
 
 const XYZ = <h1> WELCOME </h1>;
 
+const Seller = (props) =>{
+
+    return (
+    
+            <div> I am a Seller by name {props.name}</div>
+
+    )
+
+
+}
+
+// const ProductF = ( { name,price, avl }) => {
 const ProductF = (props) => {
+
+    //props destructuring
+    //const {name,price,avl} = props;
 
 
     // State hook
     const [name, setName] = useState('');
-    const [discount, setDiscount] = useState(10);
+    const [discount, FFFFF] = useState(10);
     const [inStock, setInsStock] = useState(props.avl);
+
+
+    //FFFFF(99);
 
 
     return (
@@ -21,6 +39,8 @@ const ProductF = (props) => {
             <p> value of productPrice is {props.price} </p>
             <p> Discount on this product is {discount} </p>
             <p>  Availble: {inStock === "true" ? "YES" : "NO"} </p>
+
+            <Seller name="Tulsi"></Seller>
 
         </div>
 
