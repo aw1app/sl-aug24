add9c69b55bce805b9953284112a206138b45838
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-10-12 21:30:05 +0530

    react updates redux demo

--- PHASE1/REACT/redux-demo/src/components/Counter.js
+++ PHASE1/REACT/redux-demo/src/components/Counter.js
@@ -1,15 +1,15 @@
 
-function Counter() {
+function Counter(props) {
 
     
 
     return (
         <div>
             <p> INSIDE COUNTER COMPONENT</p>
-            <h3>Count: {state.count}</h3>
+            <h3>Count: {props.count}</h3>
 
-            <button onClick={() => dispatch({ type: 'increment' })}> +1 </button>
-            <button onClick={() => dispatch({ type: 'decrement' })}> -1 </button>
+            <button onClick={props.increment}> +1 </button>
+            <button onClick={props.decrement}> -1 </button>
 
         </div>
     );
