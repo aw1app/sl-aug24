2e4f7157f84e672158ac3cb69429fa5298a9157d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-10-13 22:33:15 +0530

    redux-weather-app

---
+++ PHASE1/REACT/redux-weather-app/src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import weatherReducer from './reducers/reducers'; // Import the reducer
+
+// Create the store with middleware
+const store = createStore(weatherReducer, applyMiddleware(thunk));
+
+export default store;
+
