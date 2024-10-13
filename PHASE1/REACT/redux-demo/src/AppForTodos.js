5b879c19e368c469ba981f587e8943e0005de912
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-10-13 19:51:47 +0530

    redux combine reducers

--- PHASE1/REACT/redux-demo/src/AppForTodos.js
+++ PHASE1/REACT/redux-demo/src/AppForTodos.js
@@ -4,16 +4,18 @@ import TodoList from './components/TodoList';
 
 function AppForTodos() {
 
-  const todos = useSelector(state => state.todoAdd);
+  const todos = useSelector(state => state.todoR);
 
   const dispatch = useDispatch();
+
+  const removeTodo = (id) => dispatch ( {type:'REMOVE_TODO', payload: id } )
 
   return (
     
     <div className="App">
       <h2>Redux Demo Project</h2>
 
-       <TodoList todos={todos} removeTodo={ (id) => dispatch ( {type:'REMOVE_TODO', payload:id } ) }/>
+       <TodoList todos={todos} removeTodo={ removeTodo }/>
 
     </div>
   );
--- PHASE1/REACT/redux-demo/src/reducers/todoreducer.js
+++ PHASE1/REACT/redux-demo/src/reducers/todoreducer.js
@@ -22,3 +22,4 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
