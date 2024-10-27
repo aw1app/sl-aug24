9745a41a0849816da93c592b5514c1da9b8041f1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-10-27 21:28:32 +0530

    DSA updates selection sort

--- PHASE2/SORTING/BubbleSort.js
+++ PHASE2/SORTING/BubbleSort.js
@@ -12,7 +12,6 @@ function bubbleSort(array) {
 
     }
 
-
 }
 
 
---
+++ PHASE2/SORTING/SelectionSort.js
@@ -0,0 +1,25 @@
+function selectionSort(array) {
+    let n=array.length;
+
+    for (let i = 0; i <n ; i++) {
+
+        let maxValueIndex = 0;
+
+        for (let j = 0; j < n - i; j++) {
+
+            if (array[j] > array[maxValueIndex])
+                maxValueIndex = j;
+        };
+
+        [ array[maxValueIndex], array[n -1- i] ] = [ array[n - 1 - i], array[maxValueIndex] ];
+
+    }
+
+}
+
+const unsortedArray = [5, 2, 4, 1, 3]; 
+console.log("Before Sorting");
+console.log(unsortedArray);
+selectionSort(unsortedArray);
+console.log("AFTER selectionSort Sorting");
+console.log(unsortedArray);
