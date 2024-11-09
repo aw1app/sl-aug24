1a251e00141fb63c95853a0ab28f6c254b190b6c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-09 19:43:58 +0530

    updates

--- PHASE2/SORTING/QuickSort.js
+++ PHASE2/SORTING/QuickSort.js
@@ -24,7 +24,7 @@ function partition(array, low, high) {
     }
 
     [array[i+1], array[high]] = [array[high], array[i+1]]; // One last swap  
-    return i+1;
+    return i+1;// return the pivot index
 }
 
 
