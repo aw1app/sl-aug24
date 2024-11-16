c0c3c0185184a2a5d204285b5b7b826ba5a20e6e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-16 20:44:28 +0530

    updates

--- PHASE2/DS/QUEUE/Deque.js
+++ PHASE2/DS/QUEUE/Deque.js
@@ -41,11 +41,13 @@ class DeQue {
 }
 
 // Example usage 
+//1. Deque used as a stack
 const stack = new DeQue(); 
 stack.push(1); 
 stack.push(2); 
 console.log('Stack pop:', stack.pop()); 
  
+//2. Deque used as a queue
 const queue = new DeQue(); 
 queue.enqueue(1); 
 queue.enqueue(2); 
