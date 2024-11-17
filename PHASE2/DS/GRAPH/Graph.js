1155aa8c0cb798bb4be09cc3367676a74c398c1f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-17 22:25:14 +0530

    updates

--- PHASE2/DS/GRAPH/Graph.js
+++ PHASE2/DS/GRAPH/Graph.js
@@ -5,7 +5,6 @@ class Graph {
         this.adjacencyList = new Map();
     }
 
-
     // Function to add a vertex to the graph 
     addVertex(vertex) {
         this.vertices.push(vertex);
@@ -18,11 +17,30 @@ class Graph {
         this.adjacencyList.get(vertex2).push(vertex1);
     }
 
+    // Method for depth-first traversal 
+    dfs(startVertex, visited = new Set()){
+
+        if (!this.vertices.includes(startVertex) || visited.has(startVertex)) { 
+            return; 
+        } 
+
+        // if you are here, then the startVertex has not been visited before and therefore needs to be explored.
+        console.log(`Visited: ${startVertex}`); 
+        visited.add(startVertex); 
+
+        const neighbors = this.adjacencyList.get(startVertex);  // neighbours is an array coontaining all the vertices that are adjacent to 'startVertex'
+
+        for (const neighbor of neighbors) { 
+            this.dfs(neighbor, visited); 
+        } 
+    }
+
      // Function to display the graph 
      printGraph() { 
         for (const vertex of this.vertices) { 
             const neighbors = this.adjacencyList.get(vertex).join(', '); 
             console.log(`${vertex} -> ${neighbors}`); 
+            
         } 
     } 
 
@@ -34,7 +52,15 @@ graph.addVertex('A');
 graph.addVertex('B'); 
 graph.addVertex('C'); 
 graph.addEdge('A', 'B'); 
+
+
+graph.addVertex('D'); 
+graph.addEdge('B', 'D'); 
 graph.addEdge('B', 'C'); 
  
 console.log('Graph representation:'); 
 graph.printGraph(); 
+
+// DFS traversal
+console.log("DFS Traversal ")
+graph.dfs('A');
