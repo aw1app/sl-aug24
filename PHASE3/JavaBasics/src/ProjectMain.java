74398eda4ac7283dba0ad597c7e3ffa20c096701
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-11-23 22:37:18 +0530

    updates

---
+++ PHASE3/JavaBasics/src/ProjectMain.java
@@ -0,0 +1,28 @@
+
+public class ProjectMain {
+	
+	int age = 23;
+	String name="Johnny";
+	boolean isHappy=true;
+	
+	float f1 = 19.744f;
+	
+	double d1 = 23263526543532453143231432134.334343d;
+	
+	long myLongVar = 464564545465464466L;
+	
+	
+
+	public static void main(String[] args) {
+		
+		String str = "Hello Universe";
+		System.out.println(str);
+		
+		char x1 = 'y';
+		char x2 = '\u0634';
+		
+		System.out.println("x1 = " + x1);
+		System.out.println("x2 = " + x2);
+	}
+
+}
