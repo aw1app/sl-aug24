db46249f1f246d8271681b7be4c37b233d20e00c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-12-14 20:14:06 +0530

    updates

--- PHASE3/AirlineProject/src/demos/streams/StreamsDemo.java
+++ PHASE3/AirlineProject/src/demos/streams/StreamsDemo.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamsDemo {
@@ -51,7 +52,26 @@ public class StreamsDemo {
             .limit(2)
             .collect(Collectors.toList());
         top3Expensive.forEach(p -> System.out.println(p.getName() + ": $" + p.getPrice()));
-
+        
+        System.out.println("\n3. Statistics with Numeric Streams:");
+        
+        double minPrice = products.stream()
+        .mapToDouble(Product::getPrice)
+        .summaryStatistics().getMin();
+        
+        System.out.println("\n3. Min Price of all products: "+minPrice);
+        
+        // Matching
+        boolean hasExpensiveProducts = products.stream()
+                .anyMatch(p -> p.getPrice() > 1000);
+        
+        System.out.println("\n4. List has a product whose price is > 1000 : "+hasExpensiveProducts);
+        
+        Optional<Product> firstFurniture = products.stream()
+                .filter(p -> p.getCategory().equals("Furniture"))
+                .findFirst();
+        if(firstFurniture.isPresent())
+        System.out.println("\n5. First Furniture Product name is : "+firstFurniture.get().getName());
 	}
 
 }
