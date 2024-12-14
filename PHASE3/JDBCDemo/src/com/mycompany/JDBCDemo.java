309cc54e585787e2209c7b5847d0844ee4087e0b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-12-14 21:10:13 +0530

    updates

--- PHASE3/JDBCDemo/src/com/mycompany/JDBCDemo.java
+++ PHASE3/JDBCDemo/src/com/mycompany/JDBCDemo.java
@@ -2,25 +2,42 @@ package com.mycompany;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCDemo {
 
 	public static void main(String[] args) {
-		
-		//Step1 : Load the driver. 
-		//Above step is not needed for our Java versions
-		
-		
-		//Step 2: Create a Connection object.
+
+		// Step1 : Load the driver.
+		// Above step is not needed for our Java versions
+
 		try {
-		Connection connection =
-				DriverManager.getConnection("jdbc:mysql://localhost:3306/estore",
-				"root", "rootroot");
-		}catch (SQLException e) {
-			
+			// Step 2: Create a Connection object.
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/estore", "root",
+					"rootroot");
+
+			// Step 3: Create a Statement object.
+
+			Statement stmt = connection.createStatement();
+
+			// Step 4: Execute the SQL command.
+
+			ResultSet rs = stmt.executeQuery("SELECT * FROM estore.products");
+
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				double price = rs.getDouble("price");
+				String category = rs.getString("category");
+
+				System.out.println("id=" + id + ", Name=" + name + ", Price=" + price + ", Category=" + category);
+			}
+
+		} catch (SQLException e) {
+			System.err.println(e);
 		}
-		
 
 	}
 
