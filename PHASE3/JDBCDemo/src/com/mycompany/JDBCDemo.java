aeac3063aa2b43ba3331d92130b30be772ba63b6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-12-14 21:25:27 +0530

    updates

--- PHASE3/JDBCDemo/src/com/mycompany/JDBCDemo.java
+++ PHASE3/JDBCDemo/src/com/mycompany/JDBCDemo.java
@@ -7,16 +7,51 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCDemo {
+	
+	
+	// Insert Demo
+		public static void main(String[] args) {
 
-	public static void main(String[] args) {
+			// Step1 : Load the driver.
+			// Above step is not needed for our Java versions
+			Connection connection = null;
+			try {
+				// Step 2: Create a Connection object.
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/estore", "root", "rootroot");
+
+				// Step 3: Create a Statement object.
+
+				Statement stmt = connection.createStatement();
+
+				// Step 4: Execute the SQL command.
+				
+				String sql = "INSERT INTO products (name, price, category) VALUES('Apple Watch  3', 1100.00, 'Electronics'), ('Apple Watch  4', 1100.00, 'Electronics')";
+
+				int rowCount = stmt.executeUpdate(sql);
+				
+				System.out.println("Successfully added " + rowCount +  " rows");
+
+			} catch (SQLException e) {
+				System.err.println(e);
+			} finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.err.println(e);
+				}
+			}
+
+		}
+
+	// Query Demo
+	public static void main0(String[] args) {
 
 		// Step1 : Load the driver.
 		// Above step is not needed for our Java versions
-
+		Connection connection = null;
 		try {
 			// Step 2: Create a Connection object.
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/estore", "root",
-					"rootroot");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/estore", "root", "rootroot");
 
 			// Step 3: Create a Statement object.
 
@@ -26,7 +61,7 @@ public class JDBCDemo {
 
 			ResultSet rs = stmt.executeQuery("SELECT * FROM estore.products");
 
-			//Iterate the rows
+			// Iterate the rows
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
@@ -38,6 +73,12 @@ public class JDBCDemo {
 
 		} catch (SQLException e) {
 			System.err.println(e);
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				System.err.println(e);
+			}
 		}
 
 	}
