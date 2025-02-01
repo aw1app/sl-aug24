e1b7ca8e9abf9e464f62f932c8511a964d3528e8
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-01 20:58:32 +0530

    updates

--- PHASE4/JUNIT-DEMOS/src/test/java/com/sl/CalculatorTest.java
+++ PHASE4/JUNIT-DEMOS/src/test/java/com/sl/CalculatorTest.java
@@ -1,39 +1,57 @@
 package com.sl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	Calculator cal;
+
+	@BeforeEach
+	void myBeforeEach() {
+		System.out.println(" INSIDE myBeforeEach. This method will run before each test case in this class  ");
+		cal = new Calculator();
+	}
+	
+	@AfterEach
+	void myAfterEach() {
+		System.out.println(" INSIDE myAfterEach. This method will run after each test case in this class  ");
+		cal = null;
+	}
+
 
 	@Test
-	void testAdd() {
-		Calculator cal = new Calculator();
-		
+	void testAdd1() {
+		System.out.println(" INSIDE testAdd1 TC");
+		//Calculator cal = new Calculator();
+
 		int input1 = 4;
 		int input2 = 5;
-		
+
 		int expectedResult = 9;
-		
+
 		int result = cal.add(input1, input2); // should be 9
-		
-		assertEquals(result, expectedResult);		
+
+		assertEquals(result, expectedResult);
 	}
-	
+
 	@Test
-	void testAdd1() {
-		Calculator cal = new Calculator();
-		
+	void testAdd2() {
+		System.out.println(" INSIDE testAdd2 TC");
+		//Calculator cal = new Calculator();
+
 		int input1 = 11;
 		int input2 = -25;
-		
+
 		int expectedResult = -14;
-		
+
 		int result = cal.add(input1, input2); // should be -14
-		
-		assertEquals(result, expectedResult);		
+
+		assertEquals(result, expectedResult);
 	}
-	
-	//CHALLENGE: Write a TC for Divide method 
+
+	// CHALLENGE: Write a TC for Divide method
 
 }
