edeb89a8335d13e0a2dd98f87211cebbc39bc194
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-02 23:31:31 +0530

    updates

--- PHASE4/spring-di-annotations-based-autowiring/src/main/java/ProjectMain.java
+++ PHASE4/spring-di-annotations-based-autowiring/src/main/java/ProjectMain.java
@@ -1,18 +1,23 @@
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
+
+import com.sl.BankAccount;
+import com.sl.Main;
 
 @Configuration
 //@ComponentScan({"com.sl"})
 public class ProjectMain {
-	@Autowired
-	static Product p1;
+	
 	
 	
 
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectMain.class);
 		
+		Product p1 = (Product) context.getBean(Product.class);
 		
 		System.out.println("Product p1's name is " + p1.getName());
 		System.out.println("Product p1's desc is " + p1.getDesc());
