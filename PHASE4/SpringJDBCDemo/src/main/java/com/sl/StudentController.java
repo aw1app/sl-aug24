a9ae7111c0b7b8c8aa605220fbe2af78c7cd0e72
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-09 20:13:21 +0530

    updates

--- PHASE4/SpringJDBCDemo/src/main/java/com/sl/StudentController.java
+++ PHASE4/SpringJDBCDemo/src/main/java/com/sl/StudentController.java
@@ -1,35 +1,55 @@
 package com.sl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.sl.dao.StudentDAO;
 import com.sl.entity.Student;
 
+
 @Controller
 @RequestMapping("/students")
 public class StudentController {
-	
+
 	@Autowired
 	StudentDAO studentDAO;
-	
-	
+
 	@GetMapping("/listStudents")
 	public String getStudents(Model model) {
-		
-		//Business logic
+
+		// Business logic
 		List<Student> students = studentDAO.getAllStudents();
-		
+
 		model.addAttribute("students", students);
-		
+
 		return "list-students"; // display list-students.jsp
 	}
-
 	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String sayHello() {
+		
+		return "hi";
+	}
+
+	@GetMapping(value="/records", produces = "application/json")
+	@ResponseBody
+	public Map<String, String> getRecords() {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("key", "value");
+		map.put("foo", "bar");
+		map.put("aa", "bb");
+
+		return map;
+	}
 
 }
--- PHASE4/SpringJDBCDemo/src/main/java/com/sl/WebConfig.java
+++ PHASE4/SpringJDBCDemo/src/main/java/com/sl/WebConfig.java
@@ -1,11 +1,13 @@
 package com.sl;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
