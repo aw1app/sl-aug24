e7f6fe6890f79d21e678eb9fa76756518a1a4bcf
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-15 23:34:44 +0530

    updates

---
+++ PHASE4/spring-boot-rest-api-server/src/main/java/com/sl/controller/StudentRestController.java
@@ -0,0 +1,39 @@
+package com.sl.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sl.entity.Student;
+import com.sl.repositry.StudentRepositry;
+
+@RestController
+public class StudentRestController {
+	
+	@Autowired
+	StudentRepositry studentRepositry;
+
+	/* List all students */
+	@GetMapping("/list")
+	public List<Student> listAllStudents() {
+
+		List<Student> students = studentRepositry.findAll();
+		
+		return students;
+	}
+	
+	/* Create a student */
+	@PostMapping("/add-student")
+	public Student createStudent(@RequestBody Student stud) {
+		
+		Student createdStudent  = studentRepositry.save(stud);
+		
+		return createdStudent;
+	}
+
+}
+
---
+++ PHASE4/spring-boot-rest-api-server/src/main/java/com/sl/repositry/StudentRepositry.java
@@ -0,0 +1,20 @@
+package com.sl.repositry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sl.entity.Student;
+
+import java.util.List;
+
+
+@Repository
+public interface StudentRepositry extends JpaRepository<Student, Integer> {
+
+	
+	List<Student> findAllByFirstName(String firstName);
+	
+	List<Student> findAllByLastName(String lastName);
+	
+}
+
+
