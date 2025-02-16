769a57c0330bd407003329ff039641d476b4c33f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-16 20:31:35 +0530

    updates

---
+++ PHASE4/spring-boot-rest-api-server/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO cms.weekend_students(first_name, last_name, session_name, date_of_birth , enrollment_date) 
+	VALUES ('F1','L1','SESSION1','2000-01-01', '2020-05-04');
+INSERT INTO cms.weekend_students(first_name, last_name, session_name, date_of_birth , enrollment_date) 
+	VALUES ('F2','L2','SESSION1','2000-01-06', '2020-05-04');
