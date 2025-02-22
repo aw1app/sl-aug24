ac9eba1ddb23d821f059fc30a612730bc7bb04ed
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-22 20:57:20 +0530

    updates

--- PHASE4/spring-boot-mongodb-courses-micro-service/src/main/java/com/sl/CourseRepository.java
+++ PHASE4/spring-boot-mongodb-courses-micro-service/src/main/java/com/sl/CourseRepository.java
@@ -1,7 +1,9 @@
 package com.sl;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends MongoRepository<Course, String>{
 
 }
