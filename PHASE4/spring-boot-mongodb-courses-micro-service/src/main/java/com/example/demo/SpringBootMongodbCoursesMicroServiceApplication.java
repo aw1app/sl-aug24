bb9a3614eb408cc428ea6292bc03f657dda3a374
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-22 20:54:59 +0530

    updates

--- PHASE4/spring-boot-mongodb-courses-micro-service/src/main/java/com/example/demo/SpringBootMongodbCoursesMicroServiceApplication.java
+++ PHASE4/spring-boot-mongodb-courses-micro-service/src/main/java/com/example/demo/SpringBootMongodbCoursesMicroServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "com.sl")
 @SpringBootApplication
 public class SpringBootMongodbCoursesMicroServiceApplication {
 
