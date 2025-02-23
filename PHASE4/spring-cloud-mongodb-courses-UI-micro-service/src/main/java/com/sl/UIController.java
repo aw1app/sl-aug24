7f5887696a073d1ca50840556d64ad63c81268bf
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-23 23:50:52 +0530

    updates

--- PHASE4/spring-cloud-mongodb-courses-UI-micro-service/src/main/java/com/sl/UIController.java
+++ PHASE4/spring-cloud-mongodb-courses-UI-micro-service/src/main/java/com/sl/UIController.java
@@ -12,57 +12,57 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
-public class UIController{
-	
-	
+public class UIController {
+
 	// Connect to the courses micro service
 	// using RestTemplate
-	
+
 	@Autowired
-    private RestTemplate restTemplate;
-	
+	private RestTemplate restTemplate;
+
 	@GetMapping("/display-courses")
 	public String displayAllCourses(Model model) {
-		
+
 		try {
-		
-		List<Course> courses = restTemplate.exchange(
-				//"http://localhost:8081/courses/all", 
-				"http://spring-cloud-mongodb-courses-micro-service/courses/all",
-				HttpMethod.GET,
-				null,
-				new ParameterizedTypeReference< List<Course> >()  {}
-				).
-				getBody();
-		
-		model.addAttribute("courses", courses);
-		model.addAttribute("hasError", false);
-		}catch(Exception e) {
-			System.out.println("ERROR "+e.getMessage());
+
+			List<Course> courses = restTemplate.exchange(
+					//"http://localhost:8081/courses/all",
+					"http://spring-cloud-mongodb-courses-micro-service/courses/all",
+					HttpMethod.GET, null, new ParameterizedTypeReference<List<Course>>() {
+					}).getBody();
+
+			model.addAttribute("courses", courses);
+			model.addAttribute("hasError", false);
+		} catch (Exception e) {
+			System.out.println("ERROR " + e.getMessage());
 			model.addAttribute("hasError", true);
-		};
-		
+		}
+		;
+
 		return "courses"; // courses.jsp
-		
+
 	}
-	
-	
+
 	@GetMapping("/display-course/{id}")
 	public String displayAllCourses(@PathVariable String id, Model model) {
-		
-		Course course = restTemplate.exchange(
-				"http://spring-cloud-mongodb-courses-micro-service/courses/course/"+id, 
-				HttpMethod.GET,
-				null,
-				new ParameterizedTypeReference< Course >()  {}
-				).
-				getBody();
-		
-		model.addAttribute("course", course);
-		
+		try {
+			Course course = restTemplate
+					.exchange("http://spring-cloud-mongodb-courses-micro-service/courses/course/" + id, HttpMethod.GET,
+							null, new ParameterizedTypeReference<Course>() {
+							})
+					.getBody();
+
+			model.addAttribute("course", course);
+			model.addAttribute("hasError", false);
+		} catch (Exception e) {
+			System.out.println("ERROR " + e.getMessage());
+			System.out.println("ERROR " + e.getCause());
+			model.addAttribute("hasError", true);
+		}
+		;
+
 		return "course"; // course.jsp
-		
+
 	}
-	
-	
+
 }
